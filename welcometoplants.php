ead0972060435fe9e59fdcd594f626b3ecb00564
CaptainOnesie <connor.sortome@gmail.com>
2016-05-15 08:21:58 -0400

    Fixed selection for PHP plant welcome

--- welcometoplants.php
+++ welcometoplants.php
@@ -1,20 +1,25 @@
 <?php
 $input = ask("Welcome to talk to plant, what would you like to inquire about today?",
-array("CHOICES"=>"Lassie, Peaches, Prunes"));
+array("choices"=>"Alice, Bob, Hello", "timeout"=>30.0, "mode"=>"speech"));
 $plantnum;
-switch($input){
-    case "Lassie":
+say($input->value);
+switch($input->value){
+    case "Alice":
         $plantnum = "Plant1";
         break;
-    case "Peaches":
+    case "Bob":
         $plantnum = "Plant2";
         break;
-    case "Plums":
+    case "Hello":
         $plantnum = "Plant3";
+        break;
+    default:
+        $plantnum = "";
 }
+say($plantnum);
 $curl_handle=curl_init();
 
-curl_setopt($curl_handle,CURLOPT_URL,'https://blinding-inferno-9101.firebaseio.com/'.$plantnum->value.'.json');
+curl_setopt($curl_handle,CURLOPT_URL,'https://blinding-inferno-9101.firebaseio.com/'.$plantnum.'.json');
 curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
 
