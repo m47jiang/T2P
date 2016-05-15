81c0c857e0a2ff40877aaf048ee4275db13ae90a
martin.lai <martin9874@gmail.com>
2016-05-15 11:11:45 -0400

    Added error correction, dynamic voices and improved sentence structure

--- welcometoplants.php
+++ welcometoplants.php
@@ -12,28 +12,50 @@ curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Accept: application/json'));
 
+
+
 $plants = json_decode(curl_exec($curl_handle));
 curl_close($curl_handle);
 $choices = array();
 foreach ($plants as $key => $plant) {
     if($key != 'numPlants') {
-        array_push($choices, $plant->name);
+        $choices[$plant->name] = $plant;
     }
 }
 
-$choices_string = implode(',', $choices);
+$choices_string = implode(',', array_keys($choices));
 
-
-answer()
-$input = ask('Welcome to Talk to Plant. We have the health status of' . $plants->numPlants . 'plants. ' .  
-             'Please say the name of the plant you wish to talk to. Your plants are: ' . $choices_string, array( 
+say('Welcome to Talk to Plant. We have the health status of ' . $plants->numPlants . ' plants.', array(
+    "voice" => "Serena"));
+    
+function invalidInput() {
+    say("I'm sorry, I did not understand you.", array("voice" => "Serena"));
+}
+    
+$plant_name = ask('Please say the name of the plant you wish to talk to. Your plants are: ' . $choices_string, array( 
                  "choices" => $choices_string, 
                  "attempts" => 5,
                  "voice" => "Serena",
+                 "onBadChoice" => invalidInput,
                  ));
 
 
-say('You requested ')
+say('You requested the health status for ' . $plant_name->value . '. Please wait while we connect you to your plant.',
+   array( "voice" => "Serena") );
+   
 
-hangup()
+if ($plant_name->value == 'Napoleon') {
+    $voice = "Victor";
+}
+else {
+    $voice = "Allison";
+}
+
+say('Hi! This is ' . $plant_name->value . '. Here are my living conditions: ' .
+    'Humidity: ' . $choices[$plant_name->value]->humidity . 'percent. '. 
+    'Temperature: ' . $choices[$plant_name->value]->temperature . 'degrees Celsius. ' . 
+    'Light: ' . $choices[$plant_name->value]->light . ' lux. ' .
+    ' Overall, with these conditions, I am very happy. ' .
+    ' Please note that the current temperature is 5% below my threshold temperature value, which is ' . $choices[$plant_name->value]->temperature * 1.05 .
+    ' degrees Celsius. ', array( "voice" => $voice) );
 ?>
