968685fd5109b245dfe17468c98c8ddf2c27d21b
martin.lai <martin9874@gmail.com>
2016-05-15 09:43:36 -0400

    Added json parsing

--- welcometoplants.php
+++ welcometoplants.php
@@ -1,30 +1,39 @@
 <?php
-$input = ask("Welcome to talk to plant, what would you like to inquire about today?",
-array("choices"=>"Alice, Bob, Hello", "timeout"=>30.0, "mode"=>"speech"));
-$plantnum;
-say($input->value);
-switch($input->value){
-    case "Alice":
-        $plantnum = "Plant1";
-        break;
-    case "Bob":
-        $plantnum = "Plant2";
-        break;
-    case "Hello":
-        $plantnum = "Plant3";
-        break;
-    default:
-        $plantnum = "";
-}
-say($plantnum);
+
 $curl_handle=curl_init();
 
-curl_setopt($curl_handle,CURLOPT_URL,'https://blinding-inferno-9101.firebaseio.com/'.$plantnum.'.json');
+curl_setopt($curl_handle,CURLOPT_URL,'https://blinding-inferno-9101.firebaseio.com/.json');
 curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
+curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Accept: application/json'));
 
-$result = curl_exec($curl_handle);
-say($result);
+$plants = json_decode(curl_exec($curl_handle));
 curl_close($curl_handle);
+$choices = array();
+foreach ($plants as $key => $plant) {
+    if($key != 'numPlants') {
+        array_push($choices, $plant->name);
+    }
+}
 
+$choices_string = implode(',', $choices);
+
+
+answer()
+$input = ask('Welcome to Talk to Plant. We have the health status of' . $plants->numPlants . 'plants. ' .  
+             'Please say the name of the plant you wish to talk to. Your plants are: ' . $choices_string, array( 
+                 "choices" => $choices_string, 
+                 "attempts" => 5,
+                 "voice" => "Serena",
+                 ));
+
+
+say('You requested ')
+
+hangup()
 ?>
